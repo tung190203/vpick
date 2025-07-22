133775d8aad9fa06c4df536a627719af80808085
Nguyen Thanh Tung <tung19022003@gmail.com>
2025-07-22 14:17:07 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id(); // id (PK)
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('password_hash');
+            $table->string('avatar_url')->nullable();
+            $table->integer('vndupr_score')->default(0);
+            $table->string('tier')->nullable();
+            $table->enum('role', ['player', 'referee', 'admin'])->default('player');
+            $table->timestamps(); // created_at, updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Hello from Vue 3 in Laravel!</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  // Composition API
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
