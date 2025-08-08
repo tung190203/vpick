72877dec80555bdfab4d32a08978771bcea61795
Nguyen Thanh Tung <tung19022003@gmail.com>
2025-08-08 17:29:41 +0700

    feat: list and add tournament

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Participant extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'tournament_id',
+        'team_id',
+        'is_confirmed',
+    ];
 }
