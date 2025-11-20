36e6a327ae08b5dc06d951d36a486b606de97dba
Nguyen Thanh Tung <tung19022003@gmail.com>
2025-11-20 14:06:31 +0700

    fix

---
+++ client_secret.rb
@@ -0,0 +1,27 @@
+require 'jwt'
+
+key_file = 'key.txt'
+# ID tài khoản Apple Developer
+team_id = '6FX9L9YT45'
+# Identifier của Service ID hoặc Bundle Id của App ID
+client_id = 'com.vrplus.picki'
+# ID của Key vừa tạo ở bước 3
+key_id = 'X3ZC56G9YN'
+
+ecdsa_key = OpenSSL::PKey::EC.new IO.read key_file
+
+headers = {
+'kid' => key_id
+}
+
+claims = {
+    'iss' => team_id,
+    'iat' => Time.now.to_i,
+    'exp' => Time.now.to_i + 86400*180,
+    'aud' => 'https://appleid.apple.com',
+    'sub' => client_id,
+}
+
+token = JWT.encode claims, ecdsa_key, 'ES256', headers
+
+puts token
