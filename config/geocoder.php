85d10d46f2a55c4a23290660619bcb6a426a6ade
Nguyen Thanh Tung <tung19022003@gmail.com>
2025-12-05 13:45:05 +0700

    using google to search

--- config/geocoder.php
+++ config/geocoder.php
@@ -5,8 +5,8 @@ return [
 
     'google' => [
         'key' => env('GOOGLE_MAPS_API_KEY'),
-        'base_url' => 'https://maps.googleapis.com/maps/api/place/autocomplete/json',
-        'details_url' => 'https://maps.googleapis.com/maps/api/place/details/json',
+        'autocomplete_url' => 'https://places.googleapis.com/v1/places:autocomplete',
+        'details_url' => 'https://places.googleapis.com/v1/places/',
     ],
 
     'osm' => [
