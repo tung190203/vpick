532474e33eef6950e6cca9bcbe7fd4f797d2d231
Nguyen Thanh Tung <tung19022003@gmail.com>
2026-02-09 10:41:26 +0700

    list clubs

--- resources/js/service/club.js
+++ resources/js/service/club.js
@@ -1,8 +1,18 @@
 import axiosInstance from "@/utils/httpRequest.js";
 import {API_ENDPOINT} from "@/constants/index.js";
 
-export const getAllClubs = async () => {
-    return axiosInstance.get(`${API_ENDPOINT.CLUB}`).then((response) => response.data.data.clubs);
+export const getAllClubs = async (params = {}) => {
+    const queryParams = new URLSearchParams()
+    Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined && value !== null && value !== '') {
+      queryParams.append(key, value)
+    }
+  })
+
+  const queryString = queryParams.toString()
+  const url = `${API_ENDPOINT.CLUB}${queryString ? `?${queryString}` : ''}`
+  const { data } = await axiosInstance.get(url)
+  return data
 }
 
 export const joinRequest = async (clubId) => {
