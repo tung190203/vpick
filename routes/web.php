da10e2e960b8e42bbd417dbff00e65b48b2a6e50
Nguyen Thanh Tung <tung19022003@gmail.com>
2026-03-11 10:54:02 +0700

    refactor: rename club activity URL path from `edit-activity` to `activity`

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::middleware('crawler')->group(function () {
     Route::get('/clubs/{id}', [MetaPreviewController::class, 'club'])->where('id', '[0-9]+');
     Route::get('/tournament-detail/{id}', [MetaPreviewController::class, 'tournament'])->where('id', '[0-9]+');
     Route::get('/mini-tournament-detail/{id}', [MetaPreviewController::class, 'miniTournament'])->where('id', '[0-9]+');
-    Route::get('/clubs/{clubId}/edit-activity/{activityId}', [MetaPreviewController::class, 'clubActivity'])
+    Route::get('/clubs/{clubId}/activity/{activityId}', [MetaPreviewController::class, 'clubActivity'])
         ->where(['clubId' => '[0-9]+', 'activityId' => '[0-9]+']);
 });
 
